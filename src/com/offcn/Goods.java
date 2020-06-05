93c3f0b178ddf687a004d968e9d4abbeeccb9f1d
wangyi126hd <wangyi126hd@126.com>
2020-06-05 14:10:06 +0800

    第一次提交工程

---
+++ src/com/offcn/Goods.java
@@ -0,0 +1,13 @@
+package com.offcn;
+
+public class Goods  {
+    private String goodsName;
+
+    public String getGoodsName() {
+        return goodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName;
+    }
+}
